f1f04768f2008f5b354a035b6c8a94edd694f168
Nataliya Okuneva <nataliya_okuneva@epam.com>
2020-05-22 18:07:26 +0300

    add report

--- configs/protractor.config.js
+++ configs/protractor.config.js
@@ -1,20 +1,27 @@
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const HtmlReporter = require('protractor-beautiful-reporter');
+
 const yargs = require('yargs').argv;
 const capabilities = require('./constants/browsers_set');
 const finalSuite = require('../utils/parse_cli/parse_cli');
 
 exports.config = {
-  framework: 'jasmine',
+  framework: 'jasmine2',
   specs: yargs.set ? finalSuite : ['../specs/*.spec.js'],
   baseUrl: 'localhost',
-  onPrepare: function () {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  multiCapabilities: yargs.multi ? capabilities : [capabilities[0]],
+
+  onPrepare() {
     jasmine.getEnv().addReporter(new SpecReporter({
       displayFailuresSummary: true,
       displayFailuredSpec: true,
       displaySuiteNumber: false,
       displaySpecDuration: true
     }));
-  },
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  multiCapabilities: yargs.multi ? capabilities : [capabilities[0]]
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: `report`,
+      screenshotsSubfolder: 'images'
+    }).getJasmine2Reporter());
+  }
 }
