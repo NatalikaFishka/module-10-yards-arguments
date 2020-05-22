dcd291a462559998f914bc3b6034b83ec54376a8
Nataliya Okuneva <nataliya_okuneva@epam.com>
2020-05-22 11:50:43 +0300

    fix usue with run default set

--- utils/parse_cli/parse_cli.js
+++ utils/parse_cli/parse_cli.js
@@ -2,6 +2,6 @@ const yargs = require('yargs').argv;
 const testSuitSets = require('../../configs/constants/suites_paths');
 
 const finalSuite = [];
-yargs.set.split(' ').forEach((curr) => finalSuite.push(testSuitSets[curr]));
+if (yargs.set) yargs.set.split(' ').forEach((curr) => finalSuite.push(testSuitSets[curr]));
 
 module.exports = finalSuite;
